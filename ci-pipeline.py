41159620bd240214b0dd76aab6f5cfee112e7a18
Janitha Madushan <janithasen@gmail.com>
2022-11-27 20:24:53 +0530

    gitops configured for ci

--- ci-pipeline.py
+++ ci-pipeline.py
@@ -1,13 +1,26 @@
 import subprocess
 import time
+import fileinput
+import re
 
 GITOPS_SYNC_REPO = "C:/workspace/CMM707/sync-repos/registration-service"
+GITOPS_REPOSITORY = "C:/workspace/CMM707/argocd-example-apps"
+GITOPS_BRANCH = "master"
 
+SERVICE_NAME = "registration"
 BUILD_VERSION = ""
 
 def executeCommand(command, directory):
     return  subprocess.check_output(command, shell=True, cwd=directory)
     
+def replaceFileLine(file, startswith, repl_str):
+    with open(file, 'r+') as f:
+        lines = f.readlines()
+        f.seek(0)
+        f.truncate()
+        for l in lines:
+            f.write(repl_str if l.startswith(startswith) else l)    
+        
 def buildDockerImage():
     print("Building Docker Image....")
     executeCommand("gradle build -x test jibDockerBuild devVersion", GITOPS_SYNC_REPO)
@@ -16,6 +29,15 @@ def buildDockerImage():
     
 def updatingGitOpsRepo():
     print("Updating the GitOPs repository....")
+    executeCommand("git checkout " + GITOPS_BRANCH, GITOPS_REPOSITORY)
+    values_path = GITOPS_REPOSITORY + "/charts/" + SERVICE_NAME + "/values.yaml"
+    BUILD_VERSION = executeCommand("powershell.exe cat ./dVersion", GITOPS_SYNC_REPO).decode("utf-8")
+    replaceFileLine(values_path, "  tag:", "  tag: "+BUILD_VERSION)
+    print("Updated version in the git ops")
+    executeCommand("git add  charts/*", GITOPS_REPOSITORY)
+    executeCommand("git commit -m \"Updated Version\"", GITOPS_REPOSITORY)
+    executeCommand("git push", GITOPS_REPOSITORY)
+    
     
 def waitUntilSync():
     print("Waiting until the version is synced")
