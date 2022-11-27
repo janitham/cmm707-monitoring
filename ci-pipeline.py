9f34b9eaaffba5527f625301a5ab40e58bf2c933
Janitha Madushan <janithasen@gmail.com>
2022-11-27 18:29:19 +0530

    docker build configured

--- ci-pipeline.py
+++ ci-pipeline.py
@@ -1,11 +1,18 @@
 import subprocess
 import time
 
+GITOPS_SYNC_REPO = "C:/workspace/CMM707/sync-repos/registration-service"
+
+BUILD_VERSION = ""
+
 def executeCommand(command, directory):
     return  subprocess.check_output(command, shell=True, cwd=directory)
     
 def buildDockerImage():
     print("Building Docker Image....")
+    executeCommand("gradle build -x test jibDockerBuild devVersion", GITOPS_SYNC_REPO)
+    BUILD_VERSION = executeCommand("powershell.exe cat ./dVersion", GITOPS_SYNC_REPO).decode("utf-8")
+    print("Docker image version is %s" % BUILD_VERSION)
     
 def updatingGitOpsRepo():
     print("Updating the GitOPs repository....")
@@ -17,7 +24,7 @@ def runIntegrationTests():
     print("Executing Integration Tests....")
     
 def job():
-    result = executeCommand("git pull", r"C:\workspace\CMM707\sync-repos\registration-service")
+    result = executeCommand("git pull", GITOPS_SYNC_REPO)
     
     if 'Already up to date' in result.decode("utf-8") :
         print("Do not trigger the pipeline")    
