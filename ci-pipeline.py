ce18684b4bfcd9b3de35091895fe64470fe5c0dd
Janitha Madushan <janithasen@gmail.com>
2022-11-27 17:42:59 +0530

    initial script

--- ci-pipeline.py
+++ ci-pipeline.py
@@ -1,7 +1,19 @@
-def main():
-    print("Hello World!")
+import subprocess
+import time
+
+def executeCommand(command, directory):
+    return  subprocess.check_output(command, shell=True, cwd=directory)
+    
+def job():
+    result = executeCommand("git pull", r"C:\workspace\CMM707\sync-repos\registration-service")
+    
+    if 'Already up to date' in result.decode("utf-8") :
+        print("Do not trigger the pipeline")    
+    else:
+        print("Triggering the pipeline")
+    
 
 if __name__ == "__main__":
-    main()
-
-print("Guru99")
+    while True:
+        job()
+        time.sleep(10)
