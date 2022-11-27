cbd1457bd6f9f5378435cd04c29fe4e006d08bce
Janitha Madushan <janithasen@gmail.com>
2022-11-27 17:50:14 +0530

    initial script

--- ci-pipeline.py
+++ ci-pipeline.py
@@ -4,6 +4,18 @@ import time
 def executeCommand(command, directory):
     return  subprocess.check_output(command, shell=True, cwd=directory)
     
+def buildDockerImage():
+    print("Building Docker Image....")
+    
+def updatingGitOpsRepo():
+    print("Updating the GitOPs repository....")
+    
+def waitUntilSync():
+    print("Waiting until the version is synced")
+    
+def runIntegrationTests():
+    print("Executing Integration Tests....")
+    
 def job():
     result = executeCommand("git pull", r"C:\workspace\CMM707\sync-repos\registration-service")
     
@@ -11,6 +23,10 @@ def job():
         print("Do not trigger the pipeline")    
     else:
         print("Triggering the pipeline")
+        buildDockerImage()
+        updatingGitOpsRepo()
+        waitUntilSync()
+        runIntegrationTests()
     
 
 if __name__ == "__main__":
