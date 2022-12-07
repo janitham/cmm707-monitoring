10b08bece2be5ef361db79ea4c7ba28b740277a9
Janitha Madushan <janithasen@gmail.com>
2022-12-07 18:29:23 +0530

    cd-registration service

--- registration-cd-pipeline-test.py
+++ registration-cd-pipeline-test.py
@@ -7,7 +7,7 @@ import json
 
 GITOPS_SYNC_REPO = "C:/workspace/CMM707/sync-repos/registration-service"
 GITOPS_REPOSITORY = "C:/workspace/CMM707/implementation/argocd-gitops"
-GITOPS_BRANCH = "master"
+GITOPS_BRANCH = "test"
 
 SERVICE_NAME = "registration"
 BUILD_VERSION = ""
@@ -23,19 +23,14 @@ def replaceFileLine(file, startswith, repl_str):
         f.seek(0)
         f.truncate()
         for l in lines:
-            f.write(repl_str if l.startswith(startswith) else l)    
-        
-def buildDockerImage():
-    print("Building Docker Image....")
-    executeCommand("gradle build -x test jibDockerBuild devVersion", GITOPS_SYNC_REPO)
-    BUILD_VERSION = executeCommand("powershell.exe cat ./dVersion", GITOPS_SYNC_REPO).decode("utf-8")
-    print("Docker image version is %s" % BUILD_VERSION)
+            f.write(repl_str if l.startswith(startswith) else l)            
     
 def updatingGitOpsRepo():
     print("Updating the GitOPs repository....")
     executeCommand("git checkout " + GITOPS_BRANCH, GITOPS_REPOSITORY)
     values_path = GITOPS_REPOSITORY + "/charts/" + SERVICE_NAME + "/values.yaml"
-    BUILD_VERSION = executeCommand("powershell.exe cat ./dVersion", GITOPS_SYNC_REPO).decode("utf-8")
+    BUILD_VERSION = executeCommand("kubectl get deploy registration -n dev -o jsonpath='{.metadata.labels.version}'", GITOPS_SYNC_REPO).decode("utf-8").replace("'", "")
+    print(BUILD_VERSION)
     replaceFileLine(values_path, "  tag:", "  tag: "+BUILD_VERSION)
     print("Updated version in the git ops")
     executeCommand("git add  charts/*", GITOPS_REPOSITORY)
@@ -45,7 +40,7 @@ def updatingGitOpsRepo():
     
 def waitUntilSync():
     print("Waiting until the version is synced")
-    BUILD_VERSION = executeCommand("powershell.exe cat ./dVersion", GITOPS_SYNC_REPO).decode("utf-8").strip()
+    BUILD_VERSION = executeCommand("kubectl get deploy registration -n dev -o jsonpath='{.metadata.labels.version}'", GITOPS_SYNC_REPO).decode("utf-8").replace("'", "").strip()
     url = "http://localhost:"+DEPLOYMENT_PORT+"/actuator/info"
     while True:
         try:
@@ -66,17 +61,11 @@ def runIntegrationTests():
 def job():
     result = executeCommand("git pull", GITOPS_SYNC_REPO)
     
-    if 'Already up to date' in result.decode("utf-8") :
-        print("Do not trigger the pipeline")    
-    else:
-        print("Triggering the pipeline")
-        buildDockerImage()
-        updatingGitOpsRepo()
-        waitUntilSync()
-        runIntegrationTests()
+    print("Triggering CD pipeline...")
+    updatingGitOpsRepo()
+    waitUntilSync()
+    runIntegrationTests()
     
 
 if __name__ == "__main__":
-    while True:
-        job()
-        time.sleep(10)
+    job()
